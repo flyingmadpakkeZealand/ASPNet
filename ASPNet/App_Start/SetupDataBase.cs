d00464fd45547aed120c31afc05f7767c145a36f
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-03-31 14:25:45 +0200

    Connected to Azure DataBase.

--- ASPNet/App_Start/SetupDataBase.cs
+++ ASPNet/App_Start/SetupDataBase.cs
@@ -16,13 +16,21 @@ namespace ASPNet.App_Start
     public class SetupDataBase
     {
 
+        //public static void Setup()
+        //{
+        //    DataBases.RegisterDataBase("BaseHotel", 
+        //        @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = HotelDbtest2; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); 
+
+        //    DataBases.RegisterDataBase(BaseNames.BaseHotel, 
+        //        @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = HotelDbtest2; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        //}
+
         public static void Setup()
         {
             DataBases.RegisterDataBase("BaseHotel", 
-                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = HotelDbtest2; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); 
-
+                @"Data Source=magnusserverdb.database.windows.net;Initial Catalog=MagnusDataBase;User ID=MagnusAdmin;Password=Secret1234;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             DataBases.RegisterDataBase(BaseNames.BaseHotel, 
-                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = HotelDbtest2; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                @"Data Source=magnusserverdb.database.windows.net;Initial Catalog=MagnusDataBase;User ID=MagnusAdmin;Password=Secret1234;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
